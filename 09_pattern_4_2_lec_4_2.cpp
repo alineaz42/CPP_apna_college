186a08a6ee013fd09ce90a75d5a571723cd16ead
Ali Neaz <neazali77@gmail.com>
2022-07-04 01:40:40 +0600

    till palindromic pattern second part

---
+++ 09_pattern_4_2_lec_4_2.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int i, j;
+    // pattern 1
+    /*
+    for (i = n; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    */
+    // patern 2
+    /*
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            if ((i + j) % 2 == 0)
+            {
+                cout << 1 << " ";
+            }
+            else
+            {
+                cout << 0 << " ";
+            }
+        }
+        cout << endl;
+    }
+    */
+    // pattern 3 rombus pattern
+    /*
+    for (int i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (j = 1; j <= n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    */
+    // pattern 4
+    /*
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+    */
+    // pattern 5 pallindromic
+    /*
+    for (i = 1; i <= n; i++)
+    {
+        int j;
+        for (j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        int k = i;
+        for (; j <= n; j++)
+        {
+            cout << k--;
+        }
+        k = 2;
+        for (; j <= n + i - 1; j++)
+        {
+            cout << k++;
+        }
+        cout << endl;
+    }
+    */
+
+    return 0;
+}
