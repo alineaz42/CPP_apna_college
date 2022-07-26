3ac613eef019e4c5b822fa0cb6024dcc1023470f
Md Neaz Ali <neazali77@gmail.com>
2022-07-27 02:08:24 +0600

    sub array sum

--- 23_arrayChallanges.cpp
+++ 23_arrayChallanges.cpp
@@ -1,3 +1,43 @@
+// sum of every subarray
+#include<iostream>
+
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<=n-1;i++){
+        cin>>arr[i];
+    }
+    int curr=0;
+    for(int i=0;i<n;i++){
+        curr=0;
+        for(int j=i;j<n;j++){
+            curr=curr+arr[j];
+            cout<<curr<<endl;
+        }
+    }
+    return 0;
+}
+/*
+#include<iostream>
+// every sub array is a sub sequence but every sub sequence is not a sub array
+
+using namespace std;
+
+
+int main(){
+    //sub array and sub sequence of array
+    // sub array with n elements nC2+n=n(n+1)/2
+
+    // number of sub sequence is 2^n with n elements
+    return 0;
+}
+*/
+/*
 #include<iostream>
 
 
@@ -8,6 +48,8 @@ int max(int a,int b){
     }
     return b;
 }
+
+
 // Ex: Given an arry print the max till i
 int main(){
     int n;
@@ -23,3 +65,4 @@ int main(){
     }
     return 0;
 }
+*/
