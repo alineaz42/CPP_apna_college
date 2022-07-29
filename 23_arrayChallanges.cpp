1f01d3f73aca452434f9b8901042e8f79795ba31
Md Neaz Ali <neazali77@gmail.com>
2022-07-30 02:07:57 +0600

    add: recordBreaking

--- 23_arrayChallanges.cpp
+++ 23_arrayChallanges.cpp
@@ -1,14 +1,56 @@
-//longest arithmatic subarray
-#include<iostream>
-
-
+// record braker
+/*
+problem statement: Isyana is given the number of visitors at her local theme park on N consecutive days. The number of visitors on the i-th day is Vi.
+A day is record breaking it it satisfies both of the following conditions:
+1. The number of visitors on the day is strictly larger than the number of visitors on each of the previous days.
+2. Either it is the last day or the number of visitors on the day is strictly largeer than the number of visitors on the following day.
+Note that the very first day could be a record breaking day!
+Please help Isyana find out the number of record breaking days.
+*/
+// #include<iostream>
+#include "bits/stdc++.h"
 using namespace std;
 
 
 int main(){
-    
+    int n;
+    cin >> n;
+    // int a[n];
+    int a[n+1];
+    a[n]=-1;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    if(n==1){
+        cout<<"1"<<endl;
+        return 0;
+    }
+    int ans=0;
+    int mx=-1;
+    for(int i=0;i<n;i++){
+        if(a[i]>mx && a[i]>a[i+1]){
+            ans++;
+            cout<<a[i]<<":";
+        }
+        mx = max(mx,a[i]);
+    }
+    cout<<ans<<endl;
     return 0;
 }
+
+
+
+//longest arithmatic subarray
+// #include<iostream>
+
+
+// using namespace std;
+
+
+// int main(){
+//     int arr[5];
+//     return 0;
+// }
 
 /*
 // sum of every subarray
