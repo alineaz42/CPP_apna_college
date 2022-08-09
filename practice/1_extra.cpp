cb17d52dd0dd00a985ae237930e60c6fc4f45008
Ali Neaz <neazali77@gmail.com>
2022-08-09 13:51:08 +0600

    linearSerach practiced:

--- practice/1_extra.cpp
+++ practice/1_extra.cpp
@@ -1,16 +1,76 @@
+// searching in arrays
+// 1. linear search
 #include<iostream>
+using namespace std;
+void linearSearch(){
+    int n,k;
+    cout<<"Enter n:\n";
+    cin>>n;
+    cout<<"Enter target: \n";
+    cin>>k;
+    int arr[n];
+    cout<<"Enter the array: \n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]==k){cout<<"Target is: found at: "<<i<<endl;}
+        continue;
+    }
+    cout<<"Target is not found: ";
+}
+int main(){
+    linearSearch();
+    return 0;
+}
+
+
+
+/*
+// maximum and minimum of an array
+#include<iostream>
+#include<climits>
 using namespace std;
 
 int main(){
-    int n;cin>>n;
-    int x=1;
-    while(x<=n){
-        x *=16;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
     }
-    cout<<x<<endl;
-    x/=16;
-    cout<<x<<endl;
-    int lastDigit =n/x;
-    cout<<lastDigit<<endl;
+    int maximum = INT_MIN;
+    int minimum = INT_MAX;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>maximum){maximum=arr[i];}
+        if(arr[i]<minimum){minimum=arr[i];}
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"The maximum is: "<<maximum<<endl;
+    cout<<"The minimum is: "<<minimum<<endl;
     return 0;
 }
+*/
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;cin>>n;
+//     int x=1;
+//     while(x<=n){
+//         x *=16;
+//     }
+//     cout<<x<<endl;
+//     x/=16;
+//     cout<<x<<endl;
+//     int lastDigit =n/x;
+//     cout<<lastDigit<<endl;
+//     return 0;
+// }
